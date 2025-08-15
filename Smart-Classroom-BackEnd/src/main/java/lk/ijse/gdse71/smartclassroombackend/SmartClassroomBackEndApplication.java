8c20fc36cec773991bafb8747b38f853303b57dc
ZeenathulIlma <seenathulilma121243@gmail.com>
2025-08-15 14:58:01 +0530

    initialized

---
+++ Smart-Classroom-BackEnd/src/main/java/lk/ijse/gdse71/smartclassroombackend/SmartClassroomBackEndApplication.java
@@ -0,0 +1,13 @@
+package lk.ijse.gdse71.smartclassroombackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartClassroomBackEndApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmartClassroomBackEndApplication.class, args);
+    }
+
+}
---
+++ Smart-Classroom-BackEnd/src/test/java/lk/ijse/gdse71/smartclassroombackend/SmartClassroomBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.ijse.gdse71.smartclassroombackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartClassroomBackEndApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
