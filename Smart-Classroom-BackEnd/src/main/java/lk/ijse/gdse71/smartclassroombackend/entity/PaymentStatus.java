48a710d04e04045020a77ae62d14890723e414ee
ZeenathulIlma <seenathulilma121243@gmail.com>
2025-08-24 00:14:04 +0530

    refactor: rename ACCEPTED status to PAID in PaymentStatus enum

--- Smart-Classroom-BackEnd/src/main/java/lk/ijse/gdse71/smartclassroombackend/entity/PaymentStatus.java
+++ Smart-Classroom-BackEnd/src/main/java/lk/ijse/gdse71/smartclassroombackend/entity/PaymentStatus.java
@@ -3,6 +3,6 @@ package lk.ijse.gdse71.smartclassroombackend.entity;
 public enum PaymentStatus {
     UNPAID,     // if student hasn’t paid yet
     PENDING,    // if student paid, waiting for approval
-    ACCEPTED,   // if teacher approved
+    PAID,   // if teacher approved   (ACCEPTED)
     DECLINED,   // if teacher rejected
 }
