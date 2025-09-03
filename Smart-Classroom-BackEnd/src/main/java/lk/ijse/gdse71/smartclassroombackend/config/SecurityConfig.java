b4dae4880938cc1471c223415f8b05aa5c32b467
ZeenathulIlma <seenathulilma121243@gmail.com>
2025-09-03 21:18:01 +0530

    refactor(auth): add login functionality with cookies and JWT token handling

--- Smart-Classroom-BackEnd/src/main/java/lk/ijse/gdse71/smartclassroombackend/config/SecurityConfig.java
+++ Smart-Classroom-BackEnd/src/main/java/lk/ijse/gdse71/smartclassroombackend/config/SecurityConfig.java
@@ -53,7 +53,7 @@ public class SecurityConfig {
                 .cors(cors -> cors.configurationSource(request -> {
                     CorsConfiguration config = new CorsConfiguration();
                     config.setAllowedOrigins(List.of("http://localhost:63342", "http://127.0.0.1:5500")); // frontend origin - localhost & VS code live server
-                    config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE"));
+                    config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
                     config.setAllowedHeaders(List.of("*"));
                     config.setAllowCredentials(true);
                     return config;
